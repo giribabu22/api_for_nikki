8fee29ffb2256aaa6e2c4daa56b7a1a0d92cb67f
giribabu <102803078+giribabu22@users.noreply.github.com>
2022-12-02 14:36:58 +0530

    Update home.js

--- routes/home.js
+++ routes/home.js
@@ -15,7 +15,7 @@ routers.route('')
     })
     .post((req, res) =>{
         does_table.create(req.body).then((result) => {
-            res.redirect('/home')
+            res.redirect('/')
         }).catch((Errors) => {
             res.json({ err: Errors })
         });
